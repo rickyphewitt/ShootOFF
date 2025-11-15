784eede6e906e5a2e9e9637ceadb76e7a36230c3
ricky <rickyphewitt@gmail.com>
2025-11-14 16:57:28 -0800

    gradlew assemble task working, switched out some depreciated classes as well as some javafx code

---
+++ src/main/java/com/shootoff/Start.java
@@ -0,0 +1,33 @@
+/*
+ * ShootOFF - Software for Laser Dry Fire Training
+ * Copyright (C) 2016 phrack
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.shootoff;
+
+import com.shootoff.Main;
+
+public class Start {
+
+    /**
+     * Using this pattern ensures that we don't need the javafx
+     * as a module, see https://github.com/javafxports/openjdk-jfx/issues/236#issuecomment-426583174
+     * @param args
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
